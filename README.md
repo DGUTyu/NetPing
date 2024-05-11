a6e63cb143a5242feb0d204f8abef54961c2f33e
yujiajian <jiajian.yu@swiftpass.cn>
2024-05-11 17:39:42 +0800

    init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# NetPing
+NetPing
---
+++ app/src/main/java/com/example/aos/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.aos.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.netping.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
